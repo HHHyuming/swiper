b07b893f979efd97c9db0e354ebadb0bd8412c7d
Sway <2219659698@qq.com>
2019-06-26 23:48:23 +0800

    'error'

--- swiper/urls.py
+++ swiper/urls.py
@@ -5,18 +5,10 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 
-def login(request):
-    return HttpResponse("LOING")
-def register(request):
-    if request.method=='GET':
-        return render(request,'index.html')
-    return redirect('/login/')
-
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     url('^api/users/',include('user.urls')),
-    url('^login/',login),
-    url('^register/',register),
+
 
 ]
